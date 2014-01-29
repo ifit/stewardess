247eaf2050bb607e77e8fc7cddd598a9762319b2
dopm <alexej@ifit.com>
2014-01-29 10:27:03 -0700

    Add, begin.js; Delete, cats.js

---
+++ test/begin.js
@@ -0,0 +1,24 @@
+"use strict";
+var stewardess = require('../index')
+  ;
+
+stewardess(
+  function first(options, next) {
+    return next(options.idex > 10 ? 'skip' : null);
+  },
+  function second(options, next) {
+    options.arr.push(options.idex);
+    return next();
+  },
+  function third(options, next) {
+    return next(options.idex > 10 ? null : 'beginning');
+  }
+)
+.after(function(options) {
+  options.idex++;
+})
+.done(function(options) {
+  process.stdout.write('arr: ');
+  console.log(options.arr);
+})
+.run({arr: [], idex: 0});
